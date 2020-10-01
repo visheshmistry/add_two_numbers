a787caaaafa8c11041d842efaefc79afba6bdab8
shrameet37 <53472493+shrameet37@users.noreply.github.com>
2020-10-01 15:13:28 +0530

    Update add.cpp

--- add.cpp
+++ add.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 
 int main(){
-	int a,b;
-
-	// your code
+	int a,b,sum;
+	cout<<"enter one no. :";
+	cin>>a;
+	cout<<"enter another no. :";
+	cin>>b;
+	sum=a+b;
+	cout<<sum;
 
 	return 0;
 }
